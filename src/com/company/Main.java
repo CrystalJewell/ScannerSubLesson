88ea1f69624388c819f1c61a62d147ed54b544bd
Crystal Adkins <crystaladkins09@gmail.com>
2017-08-05 21:45:58 -0400

    Explaining the parts of initializing Scanner to accept user input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        //Accepting user input is one of the most important things we need to learn how to do.
+        //Scanner is the utility that Java gives us to do this.
+
+        Scanner input = new Scanner(System.in);
+        
+        /*
+        The line above breaks down into:
+        Scanner is the utility we would like to use.
+        When we want to use it we just want to call input;
+        To initialize this Scanner I want Java to know I need a NEW scanner.
+        This new Scanner is going to use the System.in property which is telling Java that there is going to be input (.in)
+        from the system we will need to process.
+         */
     }
 }
